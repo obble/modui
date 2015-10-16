a3f79b75bb34db542d627c1e6ccf279de109faf8
obble <mkentnotpaul@gmail.com>
2015-10-16 22:06:45 +0100

    25.5
    + simplify WSG FC logic

--- modpvp/objective.lua
+++ modpvp/objective.lua
@@ -17,18 +17,15 @@
     local f = CreateFrame'Frame'
     f:RegisterEvent'CHAT_MSG_BG_SYSTEM_ALLIANCE' f:RegisterEvent'CHAT_MSG_BG_SYSTEM_HORDE'
     f:SetScript('OnEvent', function()
+        a:Hide() h:Hide()
         if string.find(arg1, 'The Alliance Flag was picked up') then
             local t = gsub(arg1, 'The Alliance Flag was picked up by (.+)!', '%1')
             a:SetText(t)
             a:Show()
-        elseif not ai:IsShown() then
-            a:Hide()
         elseif string.find(arg1, 'The Horde flag was picked up') then
             local t = gsub(arg1, 'The Horde flag was picked up by (.+)!', '%1')
             h:SetText(t)
             h:Show()
-        elseif not hi:IsShown() then
-            h:Hide()
         end
     end)
 
