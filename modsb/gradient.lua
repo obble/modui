118fdf427954f046f886115333984983c5f59cea
obble <mkentnotpaul@gmail.com>
2015-10-16 15:23:27 +0100

    24
    + imp. gradient coverage

--- modsb/gradient.lua
+++ modsb/gradient.lua
@@ -1,11 +1,8 @@
 
-    function HealthBar_OnValueChanged(v, smooth)
-        if not v then return end
-        local r, g, b
-        local min, max = this:GetMinMaxValues()
+    local orig = {}
 
+    local gradient = function(v, f, min, max)
         if v < min or v > max then return end
-
         if (max - min) > 0 then
             v = (v - min)/(max - min)
         else
@@ -19,7 +16,22 @@
             g = v*2
         end
         b = 0
-        this:SetStatusBarColor(r, g, b)
+        f:SetStatusBarColor(r, g, b)
+    end
+
+    function HealthBar_OnValueChanged(v)
+        if not v then return end
+        local r, g, b
+        local min, max = this:GetMinMaxValues()
+        gradient(v, this, min, max)
+    end
+
+    orig.CastingBarFrame_OnUpdate = CastingBarFrame_OnUpdate
+    function CastingBarFrame_OnUpdate()
+        orig.CastingBarFrame_OnUpdate()
+        local v = CastingBarFrameStatusBar:GetValue()
+        local min, max = this:GetMinMaxValues()
+        gradient(v, this, min, max)
     end
 
     --
