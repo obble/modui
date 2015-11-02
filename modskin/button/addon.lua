069beedaffd01fca7338312c90969fa4e2e4d2e6
obble <mkentnotpaul@gmail.com>
2015-11-02 08:58:59 +0000

    40.5
    + addon skin comment + consistency

--- modskin/button/addon.lua
+++ modskin/button/addon.lua
@@ -3,12 +3,12 @@
     local f = CreateFrame'Frame'
     f:RegisterEvent'ADDON_LOADED'
     f:SetScript('OnEvent', function()
-        if arg1 == 'zBar' then
+        if arg1 == 'zBar' then          -- ZBAR
             for _, v in pairs({zBar1, zBar2, zBar3, zBar4, zBar9}) do
                 for i = 1, 12 do
                     local bu = _G[v..'Button'..i]
                     if bu then
-                        modSkin(bu, 16)
+                        modSkin(bu, 18)
                         modSkinPadding(bu, 2)
                         modSkinColor(bu, .2, .2, .2)
                     end
