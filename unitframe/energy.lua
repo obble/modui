ab22ae80621bb162b1a5c4683190830bfbfe4118
Steven Shiers <stevenshiers@gmail.com>
2016-11-18 15:40:25 +0000

    Energy - The energy spark now always stays correctly within the bounds of the player energy bar at all resolutions and UI scales. Simplified the code to make it more understandable.

--- unitframe/energy.lua
+++ unitframe/energy.lua
@@ -1,55 +1,48 @@
-
-
     if tonumber(GetCVar'modUnitFrame') == 0 then return end
 
-    local _, class = UnitClass'player'
+    local _, class = UnitClass('player')
     if not (class == 'ROGUE' or class == 'DRUID') then return end
 
-    local t    = 0
-    local last = 0
-    local tick = 0
 
-    local energy = CreateFrame('Statusbar', nil, PlayerFrameTexture:GetParent())
-    energy:SetStatusBarTexture[[Interface\TargetingFrame\UI-StatusBar]]
-    energy:SetMinMaxValues(0, 2) energy:SetValue(0)
+    local lastEnergyValue       = 0
+    local currentEnergyValue    = 0
+    local lastPulseTime         = 0
+
+    local energy = CreateFrame('Statusbar', nil, PlayerFrameManaBar)
+    energy:SetWidth(PlayerFrameManaBar:GetWidth())
     energy:SetAllPoints(PlayerFrameManaBar)
-    energy:SetStatusBarColor(0, 0, 0, 0)
 
     local spark = energy:CreateTexture(nil, 'OVERLAY')
     spark:SetTexture[[Interface\CastingBar\UI-CastingBar-Spark]]
     spark:SetWidth(32) spark:SetHeight(32)
-    spark:SetBlendMode'ADD'
+    spark:SetBlendMode('ADD')
 
     local energy_OnUpdate = function()
-        local v = mod((GetTime() - t), 2)
-        energy:SetValue(v)
+        local v = mod((GetTime() - lastPulseTime), 2)
         spark:SetPoint('CENTER', energy, 'LEFT', energy:GetWidth()*(v/2), 0)
-        tick = v
     end
 
     energy:SetScript('OnEvent', function()
         if arg1 == 'PLAYER_AURAS_CHANGED' then
             local stance = GetShapeshiftForm(true)
-            local power  = UnitPowerType'player'
+            local power  = UnitPowerType('player')
             if (class == 'DRUID' and stance == 3) or power == 3 then
                  energy:Show()
-            else energy:Hide()
+            else 
+                energy:Hide()
             end
         else
-            local p = UnitMana'player'
-        	if arg1 == 'player' then
-        		if p == last + 20 then t = GetTime() end
-        		last = p
-                energy:SetScript('OnUpdate', energy_OnUpdate)
-                energy:Show()
-            else
-                energy:SetScript('OnUpdate', nil)
-                energy:Hide()
-        	end
+            if arg1 == 'player' then
+                currentEnergyValue = UnitMana('player')
+                if currentEnergyValue == lastEnergyValue + 20 then 
+                    lastPulseTime = GetTime()
+                end
+                lastEnergyValue = currentEnergyValue
+            end
         end
     end)
 
-    energy:RegisterEvent'PLAYER_AURAS_CHANGED'
-    energy:RegisterEvent'UNIT_ENERGY'
+    energy:SetScript('OnUpdate', energy_OnUpdate)
 
-    --
+    energy:RegisterEvent('PLAYER_AURAS_CHANGED')
+    energy:RegisterEvent('UNIT_ENERGY')
