ce62b63eb0e89111b901c322ba7e1f28dd37c076
Steven Shiers <stevenshiers@gmail.com>
2016-11-19 00:17:35 +0000

    Increased the acurracy of the energy spark in relation to regen pulse rate when energy has been at 100% and UNIT_ENERGY event has not fired for a while. This problem was due to two factors first, that the original tick estimation of 2.0 did not take into account latency and second that private servers depending on hardware / core will not garantee UNIT_ENERGY events will be sent out every 2.0 seconds... to make this matter worse... we cannot even assume that they will send out UNIT_ENERGY events every for example 2.041 seconds.. as there seems to be a variance of +/- 0.05s between UNIT_ENERGY events across differen servers. This makes it extremely difficult to accurately estimate the timer. Some progress was made by taking an average of the gap between these UNIT_ENERGY events and using that to estimate the regen ticker while energy is at full.  As a matter of quality, the energy spark is hidden until the first UNIT_ENERGY event comes through. It will continue to estimate 120 seconds after the last UNIT_ENERGY event comes through and then hide itself again until UNIT_ENERGY event fires again. As I play a rogue I can tell you that quality / gameplay wise for pve and pvp this is the best condition we can get for the energy watch component

--- unitframe/energy.lua
+++ unitframe/energy.lua
@@ -4,13 +4,18 @@
     if not (class == 'ROGUE' or class == 'DRUID') then return end
 
 
-    local lastEnergyValue       = 0
-    local currentEnergyValue    = 0
-    local lastPulseTime         = 0
+    local lastEnergyValue       = 0     -- The energy value after the last regen pulse.
+    local currentEnergyValue    = 0     -- The current energy value at the time of the current regen pulse.
+    local preLastPulseTime      = 2.0   -- Time of the second to last regen pulse.
+    local lastPulseTime         = 2.0   -- Time of the last regen pulse.
+    local pulseTotal            = 0     -- Total time of all regen pulse gaps > 2.0.
+    local pulseCount            = 0     -- Total number of regen pulses > 2.0.
+    local syncNextUpdate        = false -- True if a regen pulse just ocurred and energy value will sync next frame.
 
     local energy = CreateFrame('Statusbar', nil, PlayerFrameManaBar)
     energy:SetWidth(PlayerFrameManaBar:GetWidth())
     energy:SetAllPoints(PlayerFrameManaBar)
+    energy:Hide()
 
     local spark = energy:CreateTexture(nil, 'OVERLAY')
     spark:SetTexture[[Interface\CastingBar\UI-CastingBar-Spark]]
@@ -18,8 +23,44 @@
     spark:SetBlendMode('ADD')
 
     local energy_OnUpdate = function()
-        local v = mod((GetTime() - lastPulseTime), 2)
-        spark:SetPoint('CENTER', energy, 'LEFT', energy:GetWidth()*(v/2), 0)
+        if (syncNextUpdate) then
+            local v = mod((GetTime() - lastPulseTime), 2.0)
+            spark:SetPoint('CENTER', energy, 'LEFT', (energy:GetWidth() * (v / 2.0)), 0)
+
+            local nextPulseTotalAddition = (lastPulseTime - preLastPulseTime)
+            if ((nextPulseTotalAddition > 2.0) and (nextPulseTotalAddition < 2.5)) then
+                pulseTotal = pulseTotal + (lastPulseTime - preLastPulseTime)
+                pulseCount = (pulseCount + 1)
+
+                -- -- TEST BLOCK ---------------------------
+                -- print('Sync update just ocurred')
+                -- print('time to be added to total: ' .. (lastPulseTime - preLastPulseTime))
+                -- print('pulseTotal: '.. pulseTotal)
+                -- print('pulseCount: ' .. pulseCount)
+                -- print('pulseAverage: ' .. pulseTotal / pulseCount)
+                -- -- END TESTBLOCK ------------------------
+            end
+            syncNextUpdate = false
+        else
+            if (pulseCount == 0) then
+                local v = mod((GetTime() - lastPulseTime), 2.0)
+                spark:SetPoint('CENTER', energy, 'LEFT', (energy:GetWidth() * (v / 2.0)), 0)
+            else
+                local v = mod((GetTime() - lastPulseTime), (pulseTotal / pulseCount))
+                spark:SetPoint('CENTER', energy, 'LEFT', (energy:GetWidth() * (v / (pulseTotal / pulseCount))), 0)
+
+                if ((GetTime() - lastPulseTime) > 120) then
+                    energy:Hide()
+                end
+
+                -- -- TEST BLOCK ---------------------------
+                -- print('running average mode')
+                -- print('pulseTotal: '.. pulseTotal)
+                -- print('pulseCount: ' .. pulseCount)
+                -- print('pulseAverage: ' .. pulseTotal / pulseCount)
+                -- -- END TESTBLOCK ------------------------
+            end
+        end
     end
 
     energy:SetScript('OnEvent', function()
@@ -34,8 +75,11 @@
         else
             if arg1 == 'player' then
                 currentEnergyValue = UnitMana('player')
-                if currentEnergyValue == lastEnergyValue + 20 then 
+                if currentEnergyValue == lastEnergyValue + 20 then
+                    preLastPulseTime = lastPulseTime
                     lastPulseTime = GetTime()
+                    syncNextUpdate = true
+                    energy:Show()
                 end
                 lastEnergyValue = currentEnergyValue
             end
@@ -43,6 +87,5 @@
     end)
 
     energy:SetScript('OnUpdate', energy_OnUpdate)
-
     energy:RegisterEvent('PLAYER_AURAS_CHANGED')
     energy:RegisterEvent('UNIT_ENERGY')
